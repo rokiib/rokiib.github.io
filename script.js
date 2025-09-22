cb80bd071ca242d6cb0941c58d7fc20f4f2c0614
Rokib Hasan <105203676+rokiib@users.noreply.github.com>
2025-09-22 07:57:22 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
